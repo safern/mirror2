f4617f9517dd644febd06ab54e897d94b779ba13
Santiago Fernandez Madero <safern@microsoft.com>
2025-01-30 05:39:03 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// add csharp comment
+namespace mirror1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a change");
+        }
+    }
+}
